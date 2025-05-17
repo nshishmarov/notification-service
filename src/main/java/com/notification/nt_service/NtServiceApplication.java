723132ffea414b3a9aaf71a88b0776a9d37783dc
Nikita Shishmarov <nshishmarov@diasoft.ru>
2025-05-17 18:32:50 +0300

    Initialize Spring Boot service

---
+++ src/main/java/com/notification/nt_service/NtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.notification.nt_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notification/nt_service/NtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.nt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
